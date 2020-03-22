7385e4f69a73e5b4c497c3e0520145556f787fa1
JeongHo <wwwkang8@gmail.com>
2020-03-22 12:43:12 +0900

    90s project init

---
+++ 90s/src/test/java/com/yapp/ios2/s/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.ios2.s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
