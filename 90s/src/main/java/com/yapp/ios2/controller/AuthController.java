8a0eb1aa80b68ce2d735a735a190bbebb81b0708
JANGYANG <radiata03@gmail.com>
2020-04-23 22:40:02 +0900

    JWT

--- 90s/src/main/java/com/yapp/ios2/controller/AuthController.java
+++ 90s/src/main/java/com/yapp/ios2/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-public class AuthConroller {
+public class AuthController {
 
     @Autowired
     KakaoService kakaoService;
