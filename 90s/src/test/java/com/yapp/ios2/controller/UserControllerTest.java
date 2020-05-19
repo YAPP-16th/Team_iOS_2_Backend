08e4bb8a1ec0453c567388080d5b87a5fbd63ef6
JANGYANG <radiata03@gmail.com>
2020-05-19 22:52:30 +0900

    add Login

--- 90s/src/test/java/com/yapp/ios2/controller/UserControllerTest.java
+++ 90s/src/test/java/com/yapp/ios2/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.yapp.ios2.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yapp.ios2.dto.JoinDto;
+import com.yapp.ios2.dto.LoginDto;
 import com.yapp.ios2.dto.UserDto;
 import org.junit.Before;
 import org.junit.Rule;
@@ -13,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.snippet.Attributes;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -30,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 
 @SpringBootTest
+@ActiveProfiles("dev")
 public class UserControllerTest {
     @Rule
     public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
@@ -63,7 +66,6 @@ public class UserControllerTest {
 
         ObjectMapper json = new ObjectMapper();
         String jsonString = json.writerWithDefaultPrettyPrinter().writeValueAsString(joinDto);
-//        Map<String, Object> data = json.readValue(jsonString, Map.class);
 
         mockMvc.perform(
                 post("/user/join")
@@ -76,8 +78,36 @@ public class UserControllerTest {
                         requestFields(
                                 fieldWithPath("email").description("이메일").attributes(new Attributes.Attribute("format","test@90s.com")),
                                 fieldWithPath("name").description("유저 이름"),
-                                fieldWithPath("password").description("비밀번호"),
+                                fieldWithPath("password").description("비밀번호").attributes(new Attributes.Attribute("format","카카오 로그인 시에는 null로 보내지 않아도 무관합니다.")),
                                 fieldWithPath("phone").type("String").description("핸드폰 번호").attributes(new Attributes.Attribute("format","010-1234-5678")),
+                                fieldWithPath("sosial").type("Boolean").description("카카오 로그인 여부").attributes(new Attributes.Attribute("format","true / false"))
+                        )
+                ));
+    }
+
+    @Test
+    public void 로그인() throws Exception {
+
+        LoginDto loginDto = new LoginDto();
+        loginDto.setEmail("test@90s.com");
+        loginDto.setPassword("test");
+        loginDto.setSosial(false);
+
+
+        ObjectMapper json = new ObjectMapper();
+        String jsonString = json.writerWithDefaultPrettyPrinter().writeValueAsString(loginDto);
+
+        mockMvc.perform(
+                post("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document.document(
+                        requestFields(
+                                fieldWithPath("email").description("이메일").attributes(new Attributes.Attribute("format","test@90s.com")),
+                                fieldWithPath("password").description("비밀번호").attributes(new Attributes.Attribute("format","카카오 로그인 시에는 null로 보내지 않아도 무관합니다.")),
                                 fieldWithPath("sosial").type("Boolean").description("카카오 로그인 여부").attributes(new Attributes.Attribute("format","true / false"))
                         )
                 ));
