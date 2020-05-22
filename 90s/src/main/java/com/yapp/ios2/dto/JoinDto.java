68efbf85094cb0f32805f9b77f7f55dabead8f43
JANGYANG <radiata03@gmail.com>
2020-05-22 18:19:54 +0900

    ..

--- 90s/src/main/java/com/yapp/ios2/dto/JoinDto.java
+++ 90s/src/main/java/com/yapp/ios2/dto/JoinDto.java
@@ -1,12 +1,10 @@
 package com.yapp.ios2.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class JoinDto {
--- 90s/src/main/java/com/yapp/ios2/dto/LoginDto.java
+++ 90s/src/main/java/com/yapp/ios2/dto/LoginDto.java
@@ -1,12 +1,10 @@
 package com.yapp.ios2.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDto {
