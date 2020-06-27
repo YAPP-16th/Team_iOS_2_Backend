461736943dfa83fbea0f977c3aa445a3a1066404
JANGYANG <radiata03@gmail.com>
2020-06-27 14:29:39 +0900

    성공

---
+++ 90s/src/test/java/com/yapp/ios2/TestConfig.java
@@ -0,0 +1,9 @@
+package com.yapp.ios2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.yapp.ios2")
+public class TestConfig {
+}
